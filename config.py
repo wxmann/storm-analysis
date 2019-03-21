cfa40036978bc32889f06928f8dcbe8c09e3e814
wxmann <jim.z.tang@gmail.com>
2019-03-21 01:12:23 -0700

    plot maps

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+ROOT_PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+RESOURCES_DIR = os.path.join(ROOT_PROJECT_DIR, 'resources')
+
+SHAPEFILE_DIR = os.path.join(RESOURCES_DIR, 'shp')
+
+
+def get_shp(directory, name):
+    return os.path.join(SHAPEFILE_DIR, directory, name)
