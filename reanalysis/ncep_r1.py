a4def30a9acc158f6ac5d5ef5b61909fc4b5c04d
wxmann <jim.z.tang@gmail.com>
2019-04-04 01:18:46 -0700

    plot height anomalies

--- reanalysis/ncep_r1.py
+++ reanalysis/ncep_r1.py
@@ -10,33 +10,38 @@ def _dataset_for(folder, file):
     return xr.open_dataset(url)
 
 
-def _filter_dataset_for(ds, level, datetime_):
+def _filter_dataset_for(ds, level, when):
     kw = {}
     if level:
         kw['level'] = level
-    if datetime_:
-        kw['time'] = datetime_
-    if kw:
-        return ds.sel(**kw)
-    return ds
+    if when:
+        kw['time'] = when
+    return ds.sel(**kw)
 
 
 class Daily4X(object):
     def __init__(self):
         pass
 
-    def hgt(self, year=None, level=500, datetime_=None):
-        if datetime_ is not None:
-            datetime_ = pd.Timestamp(datetime_)
-            year = datetime_.year
+    def hgt(self, year=None, level=None, when=None):
+        if when is not None:
+            when = pd.Timestamp(when)
+            year = when.year
         elif year is None:
             raise ValueError("Must supply year or datetime argument")
 
         ds = _dataset_for('ncep.reanalysis/pressure', f'hgt.{year}')
-        return _filter_dataset_for(ds, level, datetime_)
+        return _filter_dataset_for(ds, level, when)
 
-    def hgt_ltm(self):
-        return _dataset_for('ncep.reanalysis.derived/pressure', 'hgt.mon.1981-2010.ltm')
+    def hgt_ltm(self, level=None, when=None):
+        ds = _dataset_for('ncep.reanalysis.derived/pressure', 'hgt.4Xday.1981-2010.ltm')
+        ds_lev_filtered = _filter_dataset_for(ds, level=level, when=None)
+
+        when = pd.Timestamp(when)
+        month, day, hour = when.month, when.day, when.hour
+        dt = ds.time.dt
+        dt_query = (dt.month == month) & (dt.day == day) & (dt.hour == hour)
+        return ds_lev_filtered.sel(time=dt_query)
 
 
 daily4x = Daily4X()
