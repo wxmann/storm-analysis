084897429588103c40a557ee888b82990960f32d
wxmann <jim.z.tang@gmail.com>
2019-04-05 01:47:45 -0700

    query R1 multiple days

--- reanalysis/ncep_r1.py
+++ reanalysis/ncep_r1.py
@@ -2,6 +2,7 @@ from functools import lru_cache
 
 import xarray as xr
 import pandas as pd
+import numpy as np
 
 
 @lru_cache()
@@ -12,9 +13,9 @@ def _dataset_for(folder, file):
 
 def _filter_dataset_for(ds, level, when):
     kw = {}
-    if level:
+    if level is not None:
         kw['level'] = level
-    if when:
+    if when is not None:
         kw['time'] = when
     return ds.sel(**kw)
 
@@ -23,25 +24,71 @@ class Daily4X(object):
     def __init__(self):
         pass
 
-    def hgt(self, year=None, level=None, when=None):
-        if when is not None:
-            when = pd.Timestamp(when)
-            year = when.year
-        elif year is None:
-            raise ValueError("Must supply year or datetime argument")
+    def _get_times(self, when):
+        if isinstance(when, str):
+            return [pd.Timestamp(when)]
 
-        ds = _dataset_for('ncep.reanalysis/pressure', f'hgt.{year}')
-        return _filter_dataset_for(ds, level, when)
+        try:
+            iter(when)
+        except TypeError:
+            times = [pd.Timestamp(when)]
+        else:
+            times = [pd.Timestamp(ts) for ts in when]
 
-    def hgt_ltm(self, level=None, when=None):
+        return times
+
+    def hgt(self, when, level=None):
+        if isinstance(when, int):
+            return _dataset_for('ncep.reanalysis/pressure', f'hgt.{when}')
+
+        year_map = {}
+        for time_ in self._get_times(when):
+            year = time_.year
+            if year not in year_map:
+                year_map[year] = []
+            year_map[year].append(time_)
+
+        concat_datasets = []
+
+        for year in year_map:
+            ds = _dataset_for('ncep.reanalysis/pressure', f'hgt.{year}')
+            datetimes = year_map[year]
+            filtered = _filter_dataset_for(ds, level, when=datetimes)
+            concat_datasets.append(filtered)
+
+        return xr.concat(concat_datasets, dim='time')
+
+    def hgt_ltm(self, when=None, level=None):
         ds = _dataset_for('ncep.reanalysis.derived/pressure', 'hgt.4Xday.1981-2010.ltm')
         ds_lev_filtered = _filter_dataset_for(ds, level=level, when=None)
 
-        when = pd.Timestamp(when)
-        month, day, hour = when.month, when.day, when.hour
-        dt = ds.time.dt
-        dt_query = (dt.month == month) & (dt.day == day) & (dt.hour == hour)
-        return ds_lev_filtered.sel(time=dt_query)
+        if when is None:
+            return ds_lev_filtered
+
+        concat_datasets = []
+
+        # this is required because the ltm files are loaded with year=0, and hence loaded as cftimes
+        # rather than typical np times.
+        orig_times = ds_lev_filtered.time.values
+
+        for time_ in self._get_times(when):
+            # hack: reconstruct the time series for each year
+            # TODO: optimize this to do it for only unique years
+            newtimes = np.array([pd.Timestamp(year=time_.year,
+                                              month=t.month,
+                                              day=t.day,
+                                              hour=t.hour,
+                                              minute=t.minute) for t in orig_times])
+            ds_lev_filtered['time'] = newtimes
+            concat_datasets.append(ds_lev_filtered.sel(time=time_))
+
+        return xr.concat(concat_datasets, dim='time')
+
+        # when = pd.Timestamp(when)
+        # month, day, hour = when.month, when.day, when.hour
+        # dt = ds.time.dt
+        # dt_query = (dt.month == month) & (dt.day == day) & (dt.hour == hour)
+        # return ds_lev_filtered.sel(time=dt_query)
 
 
 daily4x = Daily4X()
