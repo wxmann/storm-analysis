848cb757dbc9d7e0685f709636a40bc2da947a88
wxmann <jim.z.tang@gmail.com>
2019-09-03 22:35:49 -0700

    load ibtracs TC data

---
+++ datasrcs/ibtracs.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from functools import partial
+
+from shared.workdir import savefile
+
+_DATA_DIR = 'ftp://eclipse.ncdc.noaa.gov/pub/ibtracs/v04r00/provisional/csv'
+
+
+def ibtracsv4(basin):
+    url = f'{_DATA_DIR}/ibtracs.{basin}.list.v04r00.csv'
+    save_to_local = savefile(url, in_subdir='ibtracs')
+
+    df = pd.read_csv(save_to_local.dest, skiprows=[1], parse_dates=['ISO_TIME'])
+    df.columns = map(str.lower, df.columns)
+    return df
+
+
+atlantic_tcs = partial(ibtracsv4, 'NA')
+
+epac_tcs = partial(ibtracsv4, 'EP')
+
+wpac_tcs = partial(ibtracsv4, 'WP')
